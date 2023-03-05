5636b93604ea2db6ac591a74ea9a3840588d2e6a
Jazz <JazzDevours@gmail.com>
2023-03-04 22:16:21 -0500

    add highlighting of boxes on mouseover

--- javascript.js
+++ javascript.js
@@ -2,6 +2,10 @@ const container = document.querySelector('#container');
 
 const grid = 16;
 
+function changeColor(e) {
+  e.target.classList.add('highlight');
+}
+
 for (let y = 0; y < grid; y++) {
   const divx = document.createElement('div');
   divx.classList.add('boxX');
@@ -9,6 +13,7 @@ for (let y = 0; y < grid; y++) {
   for (let x = 0; x < grid; x++) {
   const divy = document.createElement('div');
   divy.classList.add('boxY');
+  divy.addEventListener('mouseover', changeColor);
     divx.appendChild(divy)
   }
 }
