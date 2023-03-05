dcf8ea34de4380d5bb0159a6e08c3a3f03ef3f3b
Jazz <JazzDevours@gmail.com>
2023-03-04 22:59:12 -0500

    add button to change grid size

--- javascript.js
+++ javascript.js
@@ -1,19 +1,36 @@
 const container = document.querySelector('#container');
-
-const grid = 16;
+const button = document.querySelector('.btn');
+let grid = 16;
+button.addEventListener('click', () => {
+  grid = prompt("How many grids will be on each side? (max 100)", 16);
+  if (grid == NaN) return;
+  if (grid > 100) {
+    grid = 100;
+  }
+  removeGrid();
+  generateGrid(grid);
+})
 
 function changeColor(e) {
   e.target.classList.add('highlight');
 }
 
-for (let y = 0; y < grid; y++) {
-  const divx = document.createElement('div');
-  divx.classList.add('boxX');
-  container.appendChild(divx);
-  for (let x = 0; x < grid; x++) {
-  const divy = document.createElement('div');
-  divy.classList.add('boxY');
-  divy.addEventListener('mouseover', changeColor);
-    divx.appendChild(divy)
+function removeGrid() {
+  container.replaceChildren();
+}
+
+function generateGrid(gridSize) {
+  for (let y = 0; y < gridSize; y++) {
+    const divx = document.createElement('div');
+    divx.classList.add('boxX');
+    container.appendChild(divx);
+    for (let x = 0; x < gridSize; x++) {
+    const divy = document.createElement('div');
+    divy.classList.add('boxY');
+    divy.addEventListener('mouseover', changeColor);
+      divx.appendChild(divy)
+    }
   }
 }
+
+generateGrid(grid);
